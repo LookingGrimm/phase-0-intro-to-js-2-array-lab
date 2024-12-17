d1faded0bb0278370c5fd4e0c9b09bac0ed74693
jonathan <dragoiajonathan@gmail.com>
2024-12-17 15:34:44 -0500

    Final changes after completing the tasks

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  console.log("After append:", cats);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  console.log("After prepend:", cats);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  console.log("After removing last:", cats);
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  console.log("After removing first:", cats);
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+// Export cats and functions for use in tests
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat,
+};
